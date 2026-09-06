4a52bfa81e3464e8569cb0438966581c7b646eed
Mateo <262444070+Poison-CL@users.noreply.github.com>
2026-09-05 20:43:03 -0400

    .

--- inducta-chile/src/main.jsx
+++ inducta-chile/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+<<<<<<< Updated upstream
 import { ClerkProvider } from '@clerk/clerk-react'
 import { SaasProvider } from '@saas-ui/react'
 import App from './App.jsx'
@@ -13,5 +14,13 @@ createRoot(document.getElementById('root')).render(
         <App />
       </SaasProvider>
     </ClerkProvider>
+=======
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+>>>>>>> Stashed changes
   </StrictMode>,
 )
